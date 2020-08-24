9d64b60358b9760cece8180e35cc9109d4e0dc2b
karlmarx <5042021062karlmarx@gmail.com>
2020-08-23 20:27:08 -0400

    revert static

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = FastAPI(
     version="0.0.1",
     description="free webapp/api for operating yoga studios."
 )
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
