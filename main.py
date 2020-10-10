226165e85f91317c1cf1014c947a5097df3e033f
karlmarx <5042021062karlmarx@gmail.com>
2020-10-10 19:28:53 -0400

    try changing email to string

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         token_data = schemas.TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = crud.get_user_by_email(db, email=EmailStr(token_data.username))
+    user = crud.get_user_by_email(db, email=token_data.username)
     if user is None:
         raise credentials_exception
     return user
